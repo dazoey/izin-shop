a8142f951e408ee0b3ba8215dae7a3105d03b4f7
dazoey <151373427+dazoey@users.noreply.github.com>
2025-11-17 18:17:20 +0700

    prep

---
+++ izin-shop/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import postcss from './postcss.config.js'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss,
+  }
+})
