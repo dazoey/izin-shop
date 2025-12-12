2acae2d1c366e9e39272373cd9767cf5142d65bd
dazoey <151373427+dazoey@users.noreply.github.com>
2025-12-12 19:22:50 +0700

    u

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 const ProfilePage = () => {
   return (
     <div>
-      <p>Nama: Fadhlan Yuqa Tahtadika</p>
-      <p>Nim: 21120113140147</p>
-      <p>Kelompok: 10</p>
-      <p>Shift: 2</p>
+      <p>propil</p>
     </div>
   );
 };
